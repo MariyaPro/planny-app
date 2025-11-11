5ef401515e60065dca7ed9dcd8f8dde9f8974415
mprokofeva <mariaprokof@gmail.com>
2025-11-11 12:36:16 +0300

    mm

---
+++ tg-bot-planny/src/test/java/com/prokofeva/tgbotplanny/TgBotPlannyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokofeva.tgbotplanny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotPlannyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
